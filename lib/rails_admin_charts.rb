67aa097499b00fac1ca1fd51ad820bd903419703
Sergey Prokopchuk <svp@anahoret.com>
2016-02-22 13:25:13 +0200

    Add some class methods for define conditions

--- lib/rails_admin_charts.rb
+++ lib/rails_admin_charts.rb
@@ -8,29 +8,49 @@ module RailsAdminCharts
 
   module ClassMethods
 
-    def total_records_since(since = 30.days.ago)
+    def total_records_since(from = 30.days.ago, to = Date.today)
       date_created_at = "Date(#{self.table_name}.created_at)"
-      totals, before_count = self.group(date_created_at).count, self.where(date_created_at + ' < ?', since.to_date).count
+      totals, before_count = self.group(date_created_at).count, self.where(date_created_at + ' < ?', from.to_date).count
       # TODO separate MySQL/Postgres approaches using ActiveRecord::Base.connection.adapter_name or check hash key is_a? String/Date
-      (since.to_date..Date.today).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day] || totals[day.to_s] || 0) }
+      (from.to_date..to.to_date).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day] || totals[day.to_s] || 0) }
     end
 
-    def delta_records_since(since = 30.days.ago)
+    def delta_records_since(from = 30.days.ago, to = Date.today)
       date_created_at = "Date(#{self.table_name}.created_at)"
       deltas = self.group(date_created_at).count
-
-      (since.to_date..Date.today).map { |date| deltas[date] ||  deltas[date.to_s] || 0 }
+      (from.to_date..to.to_date).map { |date| deltas[date] ||  deltas[date.to_s] || 0 }
     end
 
-    def graph_data(since=30.days.ago)
-      [
-          {
-              name: model_name.plural,
-              pointInterval: 1.day * 1000,
-              pointStart: since.to_i * 1000,
-              data: self.total_records_since(since)
-          }
-      ]
+    # Class for condition
+    def class
+      #Company
+      nil
+    end
+
+    def chart_form?
+      false
+    end
+
+    def sym_for_condition
+      nil
+      #:company_id
+    end
+
+    def graph_data(from = Date.today.beginning_of_year, to = Date.today, condition = {})
+      data = nil
+      if condition.present?
+        data = self.unscoped.where(condition).delta_records_since(from, to)
+      else
+        data = self.unscoped.total_records_since(from, to)
+      end
+        [
+            {
+                name: model_name.plural,
+                pointInterval: 1.day * 1000,
+                pointStart: from.to_time.to_i * 1000,
+                data: data
+            }
+        ]
     end
 
     def xaxis
