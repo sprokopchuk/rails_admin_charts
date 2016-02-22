07106e413ad3210f4a1c1c8f05af94a3464f6b6b
Sergey Prokopchuk <svp@anahoret.com>
2016-02-22 13:26:31 +0200

    Add custom behavior for controller

--- lib/rails_admin_charts/rails_admin/config/actions/charts.rb
+++ lib/rails_admin_charts/rails_admin/config/actions/charts.rb
@@ -16,6 +16,18 @@ module RailsAdmin
           [:get, :post, :delete]
         end
 
+        register_instance_option :controller do
+          proc do
+            if params["chart_form"].present?
+              data = @abstract_model.model.graph_data(params[:chart_form][:from],
+                                                      params[:chart_form][:to],
+                                                      @abstract_model.model.sym_for_condition => params[:chart_form][:condition])
+              render partial: "chart", formats: [:js], locals: {data: data}
+            else
+              render action: @action.template_name
+            end
+          end
+        end
 
         register_instance_option :pjax? do
           false
